0c8e8d12d5d52a346847914f03ca9193f826020c
张钰洋 <zhyy2008z@outlook.com>
2017-01-17 11:43:55 +0800

    修改信息

--- BackupAssistant/Properties/AssemblyInfo.cs
+++ BackupAssistant/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BackupAssistant")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 穿越中的逍遥 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
